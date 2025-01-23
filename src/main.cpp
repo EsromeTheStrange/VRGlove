1ee9578f06378d86c5360148e0ae814302399c0e
Esrome The Strange <esromethestrange@gmail.com>
2025-01-23 11:56:12 +0900

    Nimble HID Test Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,30 @@
 #include <Arduino.h>
-#include <BleGamepad.h>
+#include <NimBLEHIDDevice.h>
+#include <NimBLEDevice.h>
 
-BleGamepad gamepad("HID Glove", "Caleb, Maika", 100);
+NimBLEHIDDevice *glove;
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("Starting BLE work!");
-    gamepad.begin();
+    Serial.println("VR Glove Initializing...");
+
+    NimBLEDevice::init("VR Glove");
+    
+    NimBLEServer *pServer = NimBLEDevice::createServer();
+    NimBLEService *pService = pServer->createService("ABCD");
+    NimBLECharacteristic *pCharacteristic = pService->createCharacteristic("1234");
+
+    glove = new NimBLEHIDDevice(pServer);
+    glove->setManufacturer("Caleb, Maika");
+    
+    pService->start();
+    pCharacteristic->setValue("Hello BLE");
+    
+    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID("ABCD"); // advertise the UUID of our service
+    pAdvertising->setName("NimBLE"); // advertise the device name
+    pAdvertising->start(); 
 }
 
 void loop() {
-  if (gamepad.isConnected())
-    {
-        Serial.println("Press buttons 5, 16 and start. Move all enabled axes to max. Set DPAD (hat 1) to down right.");
-        gamepad.press(BUTTON_5);
-        gamepad.press(BUTTON_16);
-        gamepad.pressStart();
-        gamepad.setAxes(32767, 32767, 32767, 32767, 32767, 32767, 32767, 32767);
-        gamepad.setHat1(HAT_DOWN_RIGHT);
-        // All axes, sliders, hats etc can also be set independently. See the IndividualAxes.ino example
-        delay(500);
-
-        Serial.println("Release button 5 and start. Move all axes to min. Set DPAD (hat 1) to centred.");
-        gamepad.release(BUTTON_5);
-        gamepad.releaseStart();
-        gamepad.setHat1(HAT_CENTERED);
-        gamepad.setAxes(0, 0, 0, 0, 0, 0, 0, 0);
-        delay(500);
-    }
 }
