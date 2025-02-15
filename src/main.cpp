2b1b08f898bfdc3866da1051741684059662a503
Esrome The Strange <esromethestrange@gmail.com>
2025-02-15 16:02:18 +0900

    Three Joint Readings

--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,55 @@
 
 BleGamepad gamepad("HID Glove", "Caleb, Maika", 100);
 
-#define HALL_PIN 14
+#define LED_PIN 23
+#define SEARCHING_BLINK_RATE 2000
+#define CONNECTED_BLINK_RATE 200
+
+#define JOINT_1 15
+#define JOINT_2 2
+#define JOINT_3 4
+
+long lastBlink = 0;
+bool currentLEDState = false;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Starting BLE work!");
     gamepad.begin();
 
-    pinMode(HALL_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+
+    pinMode(JOINT_1, INPUT);
+    pinMode(JOINT_2, INPUT);
+    pinMode(JOINT_3, INPUT);
+}
+
+
+void toggleLed(){
+    currentLEDState = !currentLEDState;
+    lastBlink = millis();
+    digitalWrite(LED_PIN, currentLEDState ? HIGH : LOW);
 }
 
 void loop() {
-    float hallValue = analogRead(HALL_PIN);
-    Serial.println(hallValue);
-    delay(100);
     if(gamepad.isConnected()){
-        
-    }
-    /*if (gamepad.isConnected())
-    {
-        Serial.println("Press buttons 5, 16 and start. Move all enabled axes to max. Set DPAD (hat 1) to down right.");
-        gamepad.press(BUTTON_5);
-        gamepad.press(BUTTON_16);
-        gamepad.pressStart();
-        gamepad.setAxes(32767, 32767, 32767, 32767, 32767, 32767, 32767, 32767);
-        gamepad.setHat1(HAT_DOWN_RIGHT);
-        // All axes, sliders, hats etc can also be set independently. See the IndividualAxes.ino example
-        delay(500);
+        int joint1 = analogRead(JOINT_1);
+        int joint2 = analogRead(JOINT_2);
+        int joint3 = analogRead(JOINT_3);
+        gamepad.setLeftThumb(joint1 * 8, joint2 * 8);
+        gamepad.setLeftTrigger(joint3 * 8);
+        gamepad.sendReport();
+        Serial.print(joint1);
+        Serial.print(" - ");
+        Serial.print(joint2);
+        Serial.print(" - ");
+        Serial.println(joint3);
+        delay(200);
 
-        Serial.println("Release button 5 and start. Move all axes to min. Set DPAD (hat 1) to centred.");
-        gamepad.release(BUTTON_5);
-        gamepad.releaseStart();
-        gamepad.setHat1(HAT_CENTERED);
-        gamepad.setAxes(0, 0, 0, 0, 0, 0, 0, 0);
-        delay(500);
-    }*/
+        if (millis() - lastBlink > CONNECTED_BLINK_RATE)
+            toggleLed();
+    }else{
+        if (millis() - lastBlink > SEARCHING_BLINK_RATE)
+            toggleLed();
+    }
 }
